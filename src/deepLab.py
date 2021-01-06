01359314666cd2ff2158906bfa7399967c7294e9
klyu-ironman <klyu@vt.edu>
2021-01-06 10:13:26 -0500

    checking main function

--- src/deepLab.py
+++ src/deepLab.py
@@ -14,18 +14,18 @@ import rospy
 
 
 def main():
-# Load the trained model, you could possibly change the device from cpu to gpu if
-# you have your gpu configured.
-model = torch.load(f'../../../../saved_models/var10_weights_ep18.pt', map_location=torch.device('cpu'))
+    # Load the trained model, you could possibly change the device from cpu to gpu if
+    # you have your gpu configured.
+    model = torch.load(f'../../../../saved_models/var10_weights_ep18.pt', map_location=torch.device('cpu'))
 
-# Set the model to evaluate mode
-model.eval()
+    # Set the model to evaluate mode
+    model.eval()
 
-source_image_dir = '../bridge_images/'
-destination_mask = '../predicted_masks/'
-destination_overlays = '../combined_overlays/'
+    source_image_dir = '../bridge_images/'
+    destination_mask = '../predicted_masks/'
+    destination_overlays = '../combined_overlays/'
 
-for image_name in tqdm(os.listdir(source_image_dir)):
-    print(image_name)
-    image_path = source_image_dir + image_name
-    generate_images(model, image_path, image_name, destination_mask, destination_overlays)
+    for image_name in tqdm(os.listdir(source_image_dir)):
+        print(image_name)
+        image_path = source_image_dir + image_name
+        generate_images(model, image_path, image_name, destination_mask, destination_overlays)
