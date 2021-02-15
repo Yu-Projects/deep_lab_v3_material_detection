fe116f26c13fd4504b38d8fbdd802acbe46f4e0f
Kevin Yu <klyu@vt.edu>
2021-02-15 13:24:36 -0500

    updated deepLab

--- scripts/deepLab.py
+++ scripts/deepLab.py
@@ -13,19 +13,19 @@ import torch
 import rospy
 
 
-def main():
-    # Load the trained model, you could possibly change the device from cpu to gpu if
-    # you have your gpu configured.
-    model = torch.load("../../../../saved_models/var10_weights_ep18.pt", map_location=torch.device('cpu'))
+# Load the trained model, you could possibly change the device from cpu to gpu if
+# you have your gpu configured.
+model = torch.load("../../../../saved_models/var10_weights_ep18.pt", map_location=torch.device('cpu'))
 
-    # Set the model to evaluate mode
-    model.eval()
+# Set the model to evaluate mode
+model.eval()
 
-    source_image_dir = '../bridge_images/'
-    destination_mask = '../predicted_masks/'
-    destination_overlays = '../combined_overlays/'
+source_image_dir = '../bridge_images/'
+destination_mask = '../predicted_masks/'
+destination_overlays = '../combined_overlays/'
+print("Kevin was here")
+for image_name in tqdm(os.listdir(source_image_dir)):
+    print(image_name)
+    image_path = source_image_dir + image_name
+    generate_images(model, image_path, image_name, destination_mask, destination_overlays)
 
-    for image_name in tqdm(os.listdir(source_image_dir)):
-        print(image_name)
-        image_path = source_image_dir + image_name
-        generate_images(model, image_path, image_name, destination_mask, destination_overlays)
